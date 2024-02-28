554e29ef6bbf03b648552b9985591065fc3fc445
Niek van den Bos <niekvandenbos@hotmail.com>
2024-02-28 17:46:07 +0100

    Add binding to easily pull/push commits.

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,2 +1,15 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
+function SetupFugitiveKeybindings()
+  if vim.bo.filetype == 'fugitive' then
+    vim.api.nvim_buf_set_keymap(0, 'n', 'gp', ':G pull<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'gP', ':G push<CR>', { noremap = true, silent = true })
+  end
+end
+
+vim.cmd [[
+augroup FugitiveKeybindings
+    autocmd!
+    autocmd BufEnter * lua SetupFugitiveKeybindings()
+augroup END
+]]
