a7b39fa77dec36c905cd586f44f3b391c8ec54a2
Niek van den Bos <niekvandenbos@hotmail.com>
2024-02-28 17:47:49 +0100

    Change keybinds based on preference.

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -2,8 +2,8 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
 function SetupFugitiveKeybindings()
   if vim.bo.filetype == 'fugitive' then
-    vim.api.nvim_buf_set_keymap(0, 'n', 'gp', ':G pull<CR>', { noremap = true, silent = true })
-    vim.api.nvim_buf_set_keymap(0, 'n', 'gP', ':G push<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'gpl', ':G pull<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'gps', ':G push<CR>', { noremap = true, silent = true })
   end
 end
 
