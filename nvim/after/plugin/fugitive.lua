687a05842baa3124b26143ef5b248b06f86fbb3b
Niek van den Bos <niekvandenbos@hotmail.com>
2024-02-28 17:58:28 +0100

    Remap cc and ca to be quiet.

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -3,8 +3,10 @@ vim.keymap.set("n", "<leader>gb", ':Telescope git_branches<CR>')
 
 function SetupFugitiveKeybindings()
   if vim.bo.filetype == 'fugitive' then
-    vim.api.nvim_buf_set_keymap(0, 'n', 'gpl', ':G pull<CR>', { noremap = true, silent = true })
-    vim.api.nvim_buf_set_keymap(0, 'n', 'gps', ':G push<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'gpl', ':Git pull<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'gps', ':Git push<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'cc', ':Git commit --quiet<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'ca', ':Git commit --amend --quiet<CR>', { noremap = true, silent = true })
   end
 end
 
