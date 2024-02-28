342d1b053a39fff6abb423ab7aae776dd8544509
Niek van den Bos <niekvandenbos@hotmail.com>
2024-02-28 18:01:42 +0100

    Push is also quiet now.

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<leader>gb", ':Telescope git_branches<CR>')
 function SetupFugitiveKeybindings()
   if vim.bo.filetype == 'fugitive' then
     vim.api.nvim_buf_set_keymap(0, 'n', 'gpl', ':Git pull<CR>', { noremap = true, silent = true })
-    vim.api.nvim_buf_set_keymap(0, 'n', 'gps', ':Git push<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'gps', ':Git push --quiet<CR>', { noremap = true, silent = true })
     vim.api.nvim_buf_set_keymap(0, 'n', 'cc', ':Git commit --quiet<CR>', { noremap = true, silent = true })
     vim.api.nvim_buf_set_keymap(0, 'n', 'ca', ':Git commit --amend --quiet<CR>', { noremap = true, silent = true })
   end
