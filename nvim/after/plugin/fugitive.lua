76424ad0109cc37e4b90a3fc26abe322a2b33ee0
Niek van den Bos <niekvandenbos@hotmail.com>
2024-02-28 17:52:07 +0100

    Add binding to pull up a Telescope view of git branches.

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gb", ':Telescope git_branches<CR>')
 
 function SetupFugitiveKeybindings()
   if vim.bo.filetype == 'fugitive' then
