cd139d1b7259e2f5a2bb5f35ffadf9472fc1a67b
OneBigOwnage <niekvandenbos@hotmail.com>
2024-04-22 08:38:51 +0200

    Show relative path in Lualine.

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -47,7 +47,7 @@ require('lualine').setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_c = { { 'filename', path = 1 } },
     lualine_x = { 'filetype' },
     lualine_y = {},
     lualine_z = { 'location' }
